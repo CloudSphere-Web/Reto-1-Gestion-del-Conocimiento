fb43d928ffb4fceb36517ca6b9544807ba423473
Adrian Lopez <adriiloma2005@gmail.com>
2024-10-17 17:50:56 +0200

    Cambio rama

---
+++ Codigo/view/layout/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/assets/css/footer.css" >
+</head>
+<body>
+<div class="background">
+    <div class="login-container">
+        <h2>Login</h2>
+        <form>
+            <label for="email">Email</label>
+            <input type="email" id="email" placeholder="Email">
+
+            <label for="password">Password</label>
+            <div class="password-wrapper">
+                <input type="password" id="password" placeholder="Password">
+                <a href="#">Forgot Password?</a>
+            </div>
+
+            <div class="remember-me">
+                <input type="checkbox" id="remember-me">
+                <label for="remember-me">Remember me</label>
+            </div>
+
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</div>
