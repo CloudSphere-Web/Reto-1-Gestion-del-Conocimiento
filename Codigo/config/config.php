cefa3a7139cb7084172a05e14f380deb25a24ad0
Adrian Lopez <adriiloma2005@gmail.com>
2024-10-22 08:35:22 +0200

    cambios

---
+++ Codigo/config/config.php
@@ -0,0 +1,23 @@
+<?php
+define("DB_HOST", "172.20.227.241");
+define("DB", "grupo5_2425");
+define("DB_USER", "grupo5_2425");
+define("DB_PASS", "O)r5xoLNR4Q0BFcF");
+
+define("DEFAULT_CONTROLLER", "Login");
+define("DEFAULT_ACTION", "Login");
+
+define("PAGINATION", 5);
+?>
+
+<?php
+//define("DB_HOST", "localhost");
+//define("DB", "proyecto");
+//define("DB_USER", "root");
+//define("DB_PASS", "");
+//
+//define("DEFAULT_CONTROLLER", "Login");
+//define("DEFAULT_ACTION", "login");
+//
+//define("PAGINATION", 5);
+//?>
