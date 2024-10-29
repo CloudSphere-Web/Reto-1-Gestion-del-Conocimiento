9ed7c82f8f329433fd4002a992167494a8d1a449
ZahirRiveraChacon <zahirallonso.rivera@ikasle.egibide.org>
2024-10-29 20:01:16 +0100

    cambios diseño

--- Codigo/view/admin/profileAdmin.html.php
+++ Codigo/view/admin/profileAdmin.html.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AERGIBIDE Profile</title>
-    <link rel="stylesheet" href="/Codigo/assets/css/admin_profile.css">
+    <link rel="stylesheet" href="assets/css/admin_profile.css">
 </head>
 <body>
 
